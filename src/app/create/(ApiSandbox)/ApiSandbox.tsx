747b26faa81a72ae252e08e72add03e9662a985f
jamesjd210 <jamesjd1024@gmail.com>
2023-09-27 00:13:01 -0500

    captured data from user

---
+++ src/app/create/(ApiSandbox)/ApiSandbox.tsx
@@ -0,0 +1,5 @@
+export default function ApiSandbox() {
+    return (
+        <div>The sandbox!</div>
+    );
+}
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,7 +2,8 @@
 import React, { useState, SyntheticEvent } from 'react';
 import { ApiForm } from '@/models/ApiForm.model'
 import { ApiRequest } from '@/models/ApiRequest.model';
-import { HttpMethod } from '@/models/HttpMethod.model'
+import { HttpMethod } from '@/models/HttpMethod.model';
+import ApiSandbox from '@/create/(ApiSandbox)/ApiSandbox';
 
 export default function Page() {
     //Tracking the state of the form data from user
@@ -13,20 +14,27 @@ export default function Page() {
     //Tracking if the form was submitted
     const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const [ApiRequest, setApiRequest] = useState<ApiRequest> ({
-        apiForm : formData,
-        httpMethod : HttpMethod.GET,
-        url : "",
-        headers : {"" : ""},
-        body : {"" : ""},
-    });
+    const [CurrentApiRequest, setApiRequest] = useState<ApiRequest> (
+        {
+            // Initialize with default values for ApiRequest properties
+            apiForm: {
+                textInput: '',
+                apiInput: '',
+            },
+            httpMethod: HttpMethod.GET,
+            url: '',
+            headers: {},
+            body: {},
+        }
+    );
     //Automatically store the changes to the input boxes
     function handleInputChange(e: SyntheticEvent<HTMLInputElement | HTMLTextAreaElement>) {
         const { name, value } = e.currentTarget;
         setFormData((prevData) => ({
           ...prevData,
           [name]: value,
-        }));
+        }));    
+        console.log(formData);   
     };
     
     //Track when user submits 
@@ -35,14 +43,19 @@ export default function Page() {
         event.preventDefault();
 
         //Read form data
-        const form = event.target as HTMLFormElement;
-        const formData = new FormData(form);
+        // const form = event.target as HTMLFormElement;
+        // const newFormData = new FormData(form);
 
-        const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
-        console.log(ApiRequest)
+        // const formJson = Object.fromEntries(newFormData.entries());
         setIsSubmitted(true);
-
+        setApiRequest((previousApiRequest) => ({
+            ...previousApiRequest,
+            apiForm : formData,
+            url : getUrl(),
+            httpMethod : getHttpMethod(),
+            headers : getHeaders(),
+        }));
+        console.log(CurrentApiRequest);
     }
 
     function getHttpMethod() : HttpMethod {
@@ -59,24 +72,45 @@ export default function Page() {
     }
 
     function getUrl() : string {
-        const url_pattern : RegExp = /http(s)?:\/\/([^ ]+)/;
-        const match: RegExpMatchArray | null = formData.apiInput.match(url_pattern);
+        const urlPattern : RegExp = /http(s)?:\/\/([^ ]+)/;
+        const match: RegExpMatchArray | null = formData.apiInput.match(urlPattern);
         if (match) {
             const url: string = match[0];
-            console.log(url);
+            //console.log(url);
             return url;
           } else {
             console.error('URL not found in the curl command.');
             return "error";
           }
     }
+
+    function getHeaders() : Record<string, string> {
+        // Use regular expressions to extract header values
+        const headerPattern : RegExp = /-H "(.*?)"/g;
+        const headerMatches = formData.apiInput.match(headerPattern);
+        
+        const result : Record<string,string> = {};
+
+        // Iterate through the header matches and extract the Header-Names and Header-Values
+        //This is SCUFFED pls fix 
+        if (headerMatches) {
+            for (const header of headerMatches) {
+                const header_parts = header.split(':');
+                if (header_parts.length >= 2) {
+                    const key = header_parts[0].trim().replace('-H "', '');
+                    const value = header_parts[1].trim().slice(0,-1);
+                    result[key] = value;
+                }
+            }
+        }
+        console.log(result);
+        return result;
+    }
     
     return (
     <main className="flex min-h-screen text-white flex-col items-center justify-center">
         {isSubmitted ? (
-            <div>
-                Time to test!
-            </div>
+            <ApiSandbox></ApiSandbox>
         ) : (
         <form method="post" onSubmit={handleSubmit} className = "text-center">
             <div className="mb-4">
