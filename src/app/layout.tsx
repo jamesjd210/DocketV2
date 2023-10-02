5cb36978557603c3a6236556d5112c5916240963
jamesjd210 <jamesjd1024@gmail.com>
2023-10-02 01:08:16 -0500

    fixed api to call based on user input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
+      <Header />
         <div className="flex flex-col min-h-screen overflow-hidden">
-          <Header />
           {children}
           <Banner />
         </div>
