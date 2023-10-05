14f5aaba7fbfe07626eb60791babcbab03db659e
jamesjd210 <jamesjd1024@gmail.com>
2023-10-05 11:43:20 -0500

    updated apirequest model forgot to push

--- src/app/models/ApiRequest.model.tsx
+++ src/app/models/ApiRequest.model.tsx
@@ -1,7 +1,5 @@
-import { ApiForm } from '@/models/ApiForm.model'
 import { HttpMethod } from '@/models/HttpMethod.model'
 export interface ApiRequest {
-    apiForm : ApiForm;
     httpMethod : HttpMethod;
     url : string;
     headers : Record<string, string>;
