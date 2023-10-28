8653faea5dd0b589c20f75ec4c5633e9813fedbb
jamesjd210 <jamesjd1024@gmail.com>
2023-10-27 19:43:35 -0500

    finished dynamic navbar

--- src/app/docs/retrieve/(retrievecomponents)/DynamicNavbar.tsx
+++ src/app/docs/retrieve/(retrievecomponents)/DynamicNavbar.tsx
@@ -1,19 +1,43 @@
 import { DocketObject } from '@/models/DocketObject.model';
 import { useUserDocsData } from '@/docs/retrieve/UserDocsDataProvider';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 
 export default function DynamicNavbar() {
   const { userDocsData , handleUpdateUserDocsData } = useUserDocsData();
+
+  function handleLanguageChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const endpoint = event.target.value;
+    handleUpdateUserDocsData(endpoint, userDocsData.currSubmitStatus, userDocsData.currDocketObjects);
+
+  }
+
+  const endpointOptions = userDocsData.currDocketObjects.map((currDocketObject) => (
+    <option key={currDocketObject.currApiRequest.url} value={currDocketObject.currApiRequest.url} className="py-2">
+      {currDocketObject.currApiRequest.httpMethod + " " + currDocketObject.currApiRequest.url}
+    </option>
+  ));
+
+  const endpointSelector = (
+    <div>
+        <h1 className="text-2xl font-bold mb-4">API</h1>
+        <div className='mt-4'>
+            <label className="block text-sm font-medium">Select Endpoint:</label>
+            <select
+                value={userDocsData.currEndpoint}
+                onChange={handleLanguageChange}
+                className="border p-2 rounded-md bg-white text-black shadow-md focus:outline-none w-80"
+            >
+            {endpointOptions}
+            </select>
+        </div>
+    </div>
+  );
+
   return (
-    <nav className="text-left">
-      <ul>
-        {userDocsData.currDocketObjects.map((currDocketObject: DocketObject) => (
-          <li key={currDocketObject._id}>
-            <a>{currDocketObject.currApiRequest.url}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+    {endpointSelector}
+    </>
   );
 }
